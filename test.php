ebb1b95fa777557b7fee67c1305cc85fb1541aa8
Giorgi <45801652+Barbaqadze@users.noreply.github.com>
2022-08-11 14:00:24 +0400

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,1 @@
-var xhr = new XMLHttpRequest();
-xhr.open("POST", 'http://85.90.246.239:2222/testread.php', true);
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-xhr.onreadystatechange = function() { // Call a function when the state changes.
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // Request finished. Do processing here.
-    }
-}
-xhr.send("handler=localhost");
+readfile('http://localhost');
